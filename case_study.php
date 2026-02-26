d832a8645940dbf50fcde6c85fd913753c35d84a
Zeran Lu <zeranlu0326@gmail.com>
2026-02-25 20:35:58 -0500

    created case_study.php
    in this commit, a page for case studies was created

---
+++ case_study.php
@@ -0,0 +1,41 @@
+<?php require_once 'includes/connect.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Case Studies - Zeran Lu Portfolio</title>
+    <link type="text/css" href="css/main.css" rel="stylesheet">
+    <link type="text/css" href="css/grid.css" rel="stylesheet">
+</head>
+<body>
+    <header class="grid-con">
+    
+        <button id="hamburger">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#C59898"><path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>
+        </button>
+
+        <div class="overlay" id="menu">
+
+            <button id="close">
+                <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#C59898"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            </button>
+
+            <nav>
+                <ul>
+                    <li><a href="index.html"><img src="images/gen-logo/port-logo.svg" alt="logo"></a></li>
+                    <li><a href="#about"><span>01.</span> About</a></li>
+                    <li><a href="#projects"><span>02.</span> Projects</a></li>
+                    <li><a href="#skills"><span>03.</span> Skills</a></li>
+                    <li><a href="case_study.php"><span>04.</span> Case Studies</a></li>
+                    <li><a href="contact.html"><span>05.</span> Contact</a></li>
+                    <li><button class="btn"><a href="assets/lu_zeran_resume.pdf">Resume</a></button></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    
+</body>
+</html>
