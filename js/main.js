9ed9a915b209b25b6f7ac4103cdef329b50332cb
Zeran Lu <zeranlu0326@gmail.com>
2026-02-28 21:56:19 -0500

    added animated text JS module

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 import { hamburgerMenu } from "./modules/hamburgerMenu.js";
 import { scrollTo } from "./modules/scrollTo.js";
 import { contactForm } from "./modules/contactForm.js";
-
+import { specialText } from "./modules/specialText.js";
 
 hamburgerMenu();
 scrollTo();
 
 if (document.body.dataset.page === "home") {
     console.log("welcome to the homepage of my portfolio!");
+
+    specialText();
 
 } else if (document.body.dataset.page === "case-study") {
     console.log("check out some case studies on my projects");
