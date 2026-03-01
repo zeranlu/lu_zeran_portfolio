739d7c439b8389657f7791752d8734499e5d595e
Zeran Lu <zeranlu0326@gmail.com>
2026-02-28 21:35:42 -0500

    deleted redundant scrolltrigger js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { hamburgerMenu } from "./modules/hamburgerMenu.js";
 import { scrollTo } from "./modules/scrollTo.js";
-import { scrollTriggerEffect } from "./modules/scrollTrigger.js";
 import { contactForm } from "./modules/contactForm.js";
 
 
@@ -9,9 +8,6 @@ scrollTo();
 
 if (document.body.dataset.page === "home") {
     console.log("welcome to the homepage of my portfolio!");
-
-
-    // scrollTriggerEffect();
 
 } else if (document.body.dataset.page === "case-study") {
     console.log("check out some case studies on my projects");
